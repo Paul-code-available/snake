f64b4b54d53e9ceeebe7932e997c1e6911d3aa30
Paul-code-available <paulsanchezsolisvega10@gmail.com>
2026-03-18 09:24:46 -0700

    Panel de puntaje

--- snake/GamePanel.java
+++ snake/GamePanel.java
@@ -2,11 +2,14 @@ package snake;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Panel;
 import java.awt.Point;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Iterator;
 import java.util.LinkedList;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
@@ -23,6 +26,9 @@ public class GamePanel extends JPanel {
 	// Timer que controla el ciclo del juego
 	Timer timer;
 
+	// contador de comidas
+	int contadorComidas = 0;
+	
 	// Coordenadas de la cabeza de la serpiente
 	int snakeX;
 	int snakeY;
@@ -57,9 +63,6 @@ public class GamePanel extends JPanel {
 
 		// Crear la lista que almacenará el cuerpo de la serpiente
 		snakeBody = new LinkedList<Point>();
-		
-		// crea la lista que solo almacenará una manzana
-		almacenComida = new LinkedList<Point>();
 
 		// Agregar los primeros segmentos de la serpiente
 		snakeBody.add(new Point(250, 100));
@@ -68,9 +71,20 @@ public class GamePanel extends JPanel {
 
 		// Configurar tamaño del panel
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setLayout(null);
 
 		// Color de fondo
 		this.setBackground(Color.black);
+		
+		// panel de puntaje
+		Panel panelPuntaje = new Panel();
+		panelPuntaje.setBounds(550, 0, 70, 50);
+		panelPuntaje.setBackground(Color.BLUE);
+		add(panelPuntaje);
+		
+		// etiqueta de puntaje
+		JLabel lblPuntaje = new JLabel("Puntaje: " + contadorComidas);
+		panelPuntaje.add(lblPuntaje);
 
 		// Permitir que el panel reciba eventos de teclado
 		this.setFocusable(true);
@@ -213,6 +227,7 @@ public class GamePanel extends JPanel {
 			comidaY = (int) (Math.random() * 23 + 1) * 25;
 			
 			snakeBody.add(new Point(snakeBody.getLast().x - 25, snakeBody.getLast().y));
+			contadorComidas += 1;
 			
 		}
 	}
